ede2f18d5a2c8a38613ea7a5cde28223ad63b1c9
Maynot2 <paulmanot@gmail.com>
2021-02-16 18:06:21 +0000

    Add test file for file_storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Contains test cases for BasicModel objects
+"""
+import unittest
+import models
+BaseModel = models.base_model.BaseModel
+
+
+class TestBase(unittest.TestCase):
+    """
+        Tests for FileStorage object
+    """
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
