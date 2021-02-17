b8745d3d30f48ef8bacc93ec9a6428811d5b7ff4
Maynot2 <paulmanot@gmail.com>
2021-02-17 08:12:23 +0000

    Add State class

---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Contains the User class
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+        Instantiates a new State with the given:
+        # Public class attributes:
+            name: string
+    """
+
+    name = ''
