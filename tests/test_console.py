f3a4b48a028b1eff817a63fd4daf9a75dba3a635
Maynot2 <paulmanot@gmail.com>
2021-02-17 21:33:38 +0000

    Add test files for console

---
+++ tests/test_console.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Contains test cases for the Console
+"""
+import unittest
+import models
+BaseModel = models.base_model.BaseModel
+
+
+class TestBase(unittest.TestCase):
+    """
+        Tests for the Console
+    """
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
