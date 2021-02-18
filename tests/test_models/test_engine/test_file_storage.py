b1455bb3503cf8c66d93afec0936d4d764cdaba8
Maynot2 <paulmanot@gmail.com>
2021-02-18 09:07:22 +0000

    Add test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,8 +5,9 @@
 import unittest
 import models
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
-class TestBase(unittest.TestCase):
+class TestFileStorage(unittest.TestCase):
     """
         Tests for FileStorage object
     """
@@ -16,8 +17,17 @@ class TestBase(unittest.TestCase):
         self.fs._FileStorage__objects = {}
 
     def test_has_private_attribute_file_path(self):
-        print(self.fs.__dict__)
         self.assertTrue(hasattr(self.fs, '_FileStorage__file_path'))
+
+    def test_has_private_attribute_objects(self):
+        self.assertTrue(hasattr(self.fs, '_FileStorage__objects'))
+
+    def test_method_all_retrieves_all_the_stored_object(self):
+        b1 = BaseModel()
+        b2 = BaseModel()
+        b3 = BaseModel()
+        self.assertEqual(len(self.fs.all(self.fs).keys()), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
